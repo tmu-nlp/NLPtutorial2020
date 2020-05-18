211f0187db0732c2bf42bd6e9d1a973a92ed0fb1
10-Is-Mana <10suism.ashida@gmail.com>
2020-05-18 20:05:10 +0900

    add lines

--- mana/tutorial00/tutorial00.py
+++ mana/tutorial00/tutorial00.py
@@ -14,7 +14,10 @@ for line in testLines:
         else:
             counts[word] = 1
  
+#no. of unique words
 print("the number of unique words in a given text is " + str(len(counts)))
+
+#word frequency in alphabetical order
 print("a list of words in a given text by frequency is")
 for key, value in sorted(counts.items()):
-    print('%s --> %r' % (key, value))
+    print('%s --> %r' % (key, value))      #homage to Graham sensei's style using arrows in the slide
