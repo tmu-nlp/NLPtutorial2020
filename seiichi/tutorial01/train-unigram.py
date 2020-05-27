49e484b32f3e43fd338af7cd3c38d85bf990444a
Seiichi Inoue <d35inou108@gmail.com>
2020-05-27 10:35:16 +0900

    done tutorial01

---
+++ seiichi/tutorial01/train-unigram.py
@@ -0,0 +1,19 @@
+# train 1-gram model
+import os, sys
+import math
+import pickle
+import numpy as np
+if not os.path.exists("./model"): os.mkdir("./model")
+
+from unigram import *
+
+
+if __name__ == "__main__":
+    # train_path = sys.argv[1]
+    # train_path = "../../test/01-train-input.txt"
+    # test_path = "../../test/01-test-input.txt"
+    train_path = "../../data/wiki-en-train.word"
+    model = UnigramModel()
+    model.train(train_path)
+    model.save("./model/wikien.model")
+    # model.word_probabilities()
