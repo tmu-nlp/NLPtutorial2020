6333661bbb1045dc5b3d57e66df57bf5e7d52dac
Seiichi Inoue <d35inou108@gmail.com>
2020-06-24 05:11:33 +0900

    fixed

--- seiichi/common/functions.py
+++ seiichi/common/functions.py
@@ -17,7 +17,7 @@ def softmax(x):
     return x
 
 def cross_entropy_error(y, t):
-    if t.ndim == 2:
+    if t.ndim == 2 and t[0].size == 1:
         t = t.reshape(t.size)
     if y.ndim == 1:
         t = t.reshape(1, t.size)
