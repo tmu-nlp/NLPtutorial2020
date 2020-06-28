acd72a279513b3eaf1d91c166ca9f602b1171229
a1da4 <ai.the.taichi@gmail.com>
2020-06-28 20:03:43 +0900

    fixed

--- aida/tutorial05/tutorial05.py
+++ aida/tutorial05/tutorial05.py
@@ -1,16 +1,15 @@
 from collections import defaultdict
-from pprint import pprint
 
 class Perceptron():
     def __init__(self):
         """
-        :param w: dict, weight for each feature
+        :param w: defaultdict, weight for each feature
         """
-        self.w = {}
+        self.w = defaultdict(lambda: 0)
 
     def create_features(self, sentence):
         """ obtain unigram features
-        :param phi: defaultdict
+        :param phi: defaultdict, count features
         """
         phi = defaultdict(lambda: 0)
         words = sentence.split()
@@ -18,14 +17,6 @@ class Perceptron():
             phi['UNI:{}'.format(word)] += 1
         return phi
 
-    def initialize_weights(self, phi):
-        """ initialize weights
-        """
-        for feature in phi.keys():
-            if feature not in self.w:
-                self.w[feature] = 0
-        return self
-    
     def update_weights(self, phi, ans):
         """ update weights
         """
@@ -43,34 +34,31 @@ class Perceptron():
         """
         score = 0
         for feature, value in phi.items():
-            if feature in self.w:
-                score += value * self.w[feature]
+            score += value * self.w[feature]
         if score >= 0:
             y_hat = 1
         else:
             y_hat = -1
         return y_hat
 
-    def train_one(self, sentence, ans, n_iter):
-        """ online training
+    def train_one(self, sentence, ans):
+        """ training one sentence
         """
-        for n in range(n_iter):
-            phi = self.create_features(sentence)
-            self.initialize_weights(phi)
-            y_hat = self.predict_one(phi)
-            if ans != y_hat:
-                self.update_weights(phi, ans)
+        phi = self.create_features(sentence)
+        y_hat = self.predict_one(phi)
+        if ans != y_hat:
+            self.update_weights(phi, ans)
 
     def fit(self, file_path, n_iter):
         """ training all dataset
         :param file_path: a path of training dataset
         :param n_iter: int, iteration
         """
-        with open(file_path) as fp:
-            for line in fp:
-                ans, sentence = line.strip().split('\t')
-                #self.train_online(sentence, ans, n_iter)
-                self.train_one(sentence, int(ans), n_iter)
+        for _ in range(n_iter):
+            with open(file_path) as fp:
+                for line in fp:
+                    ans, sentence = line.strip().split('\t')
+                    self.train_one(sentence, int(ans))
 
     def predict(self, input_file):
         """ predict all sentences
@@ -87,8 +75,9 @@ if __name__ == '__main__':
     train_file = './../data/titles-en-train.labeled'
     perceptron = Perceptron()
     perceptron.fit(train_file, n_iter=10)
-    #pprint(perceptron.w)
+    with open('./weights.txt', 'w') as fp:
+        for feature, value in perceptron.w.items():
+            fp.write('{}\t{}\n'.format(feature, value))
     test_file = './../data/titles-en-test.word'
     perceptron.predict(test_file)
-
 
